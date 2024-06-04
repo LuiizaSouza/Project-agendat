1009ff35f70d5c94631209bd243b03726fb6a0ea
LuiizaSouza <luizagmes55@gmail.com>
2024-06-04 16:33:32 -0300

    Update Contact_form.dart

--- lib/app/View/Contact_form.dart
+++ lib/app/View/Contact_form.dart
@@ -5,7 +5,7 @@ class ContactForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(title:Text('Cadastro de contatoo'),)
+      appBar:AppBar(title:Text('Cadastro de contato'),)
     );
   }
 }
